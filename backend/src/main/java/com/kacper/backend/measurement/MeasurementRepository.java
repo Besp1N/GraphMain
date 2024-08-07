cfcbb33affdcb63017ecd0fab08732dc7eb28bdf
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-08-07 21:39:28 +0200

    updated DeviceService

--- backend/src/main/java/com/kacper/backend/measurement/MeasurementRepository.java
+++ backend/src/main/java/com/kacper/backend/measurement/MeasurementRepository.java
@@ -1,6 +1,7 @@
 package com.kacper.backend.measurement;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer>
 {
-    Page<Measurement> findAllBySensorId(Integer sensorId, org.springframework.data.domain.Pageable pageable);
+    Page<Measurement> findAllBySensorId(Integer sensorId, Pageable pageable);
 }
