ae6e971edd23b90a1abac8b8be6ea1c5257f6826
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-08-07 13:22:16 +0200

    javadoc for measurement (psr-12)

--- backend/src/main/java/com/kacper/backend/measurement/MeasurementController.java
+++ backend/src/main/java/com/kacper/backend/measurement/MeasurementController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * Measurement controller class.
+ *
+ * @author Kacper Karabinowski
  */
 @RestController
 @RequestMapping("/api/measurement")
@@ -11,10 +13,20 @@ public class MeasurementController
 {
     private final MeasurementService measurementService;
 
+    /**
+     * @param measurementService is injected automatically by Spring
+     */
     public MeasurementController(MeasurementService measurementService) {
         this.measurementService = measurementService;
     }
 
+    /**
+     * Adds a measurement to the database.
+     *
+     * @param sensorId is the id of the sensor
+     * @param measurementRequest is the request body
+     * @return Measurement
+     */
     @PostMapping("/{sensorId}")
     public Measurement addMeasurement(
             @PathVariable Integer sensorId,
@@ -23,6 +35,12 @@ public class MeasurementController
         return measurementService.addMeasurement(sensorId, measurementRequest);
     }
 
+    /**
+     * Deletes a measurement from the database.
+     *
+     * @param measurementId is the id of the measurement
+     * @return Measurement
+     */
     @DeleteMapping("/{measurementId}")
     public Measurement deleteMeasurement(@PathVariable Integer measurementId) {
         return measurementService.deleteMeasurement(measurementId);
--- backend/src/main/java/com/kacper/backend/measurement/MeasurementRepository.java
+++ backend/src/main/java/com/kacper/backend/measurement/MeasurementRepository.java
@@ -2,6 +2,9 @@ package com.kacper.backend.measurement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Measurement repository
+ */
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer>
 {
 }
