aab68f7961749a46052f337a3962be00ba07440d
Sabina Kubiyeva <kubiyevasabina@gmail.com>
2024-09-10 14:17:19 +0200

    Updated AuthServiceTest

--- backend/src/test/java/com/kacper/backend/auth/AuthServiceTest.java
+++ backend/src/test/java/com/kacper/backend/auth/AuthServiceTest.java
@@ -1,8 +1,10 @@
 package com.kacper.backend.auth;
 
 import com.kacper.backend.exception.InvalidCredentialsException;
+import com.kacper.backend.exception.ResourceAlreadyExistException;
 import com.kacper.backend.jwt.JWTService;
 import com.kacper.backend.user.User;
+import com.kacper.backend.mail.MailService;
 import com.kacper.backend.user.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,6 +41,12 @@ class AuthServiceTest {
 
     @InjectMocks
     private AuthService authService;
+
+    @Mock
+    private MailService mailService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     /**
      * Set up before each test
@@ -107,4 +117,50 @@ class AuthServiceTest {
         // Act & Assert
         assertThrows(InvalidCredentialsException.class, () -> authService.login(authLoginRequest));
     }
+
+
+
+    @Test
+    void register_alreadyExists() {
+
+        AuthRegistrationRequest authRegistrationRequest = new AuthRegistrationRequest("sobaka@example.com", "password", "So", "Baka", "user");
+        when(userRepository.save(any(User.class))).thenThrow(new ResourceAlreadyExistException("User with email: sobaka@example.com already exists"));
+
+        assertThrows(ResourceAlreadyExistException.class, () -> authService.register(authRegistrationRequest));
+
+
+        verify(userRepository).save(any(User.class));
+        verify(mailService, never()).sendMail(anyString());
+    }
+
+
+    @Test
+    void registerCapitalizes() {
+        AuthRegistrationRequest authRegistrationRequest = new AuthRegistrationRequest(
+                "test678@example.com", "password", "soba", "kao", "user");
+
+        User user = User.builder()
+                .email("test678@example.com")
+                .password("encodedPassword")
+                .name("Soba")
+                .lastName("Kao")
+                .role("USER")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(passwordEncoder.encode(authRegistrationRequest.password())).thenReturn("encodedPassword");
+        AuthRegistrationResponse response = authService.register(authRegistrationRequest);
+
+        assertNotNull(response);
+        assertEquals("Soba", response.name());
+        assertEquals("Kao", response.lastName());
+        assertEquals("test678@example.com", response.email());
+        assertEquals("USER", response.role());
+
+        verify(userRepository).save(any(User.class));
+        verify(mailService).sendMail("test678@example.com");
+    }
+
+
 }
